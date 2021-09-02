9609db5f6e710b76b641199ded93dba8df7b5ab4
SanzidaSuchi <sanzidasuchi356@gmail.com>
2021-09-02 19:05:46 +0600

    all

--- js/book.js
+++ js/book.js
@@ -10,30 +10,44 @@ document.getElementById('search-btn').addEventListener('click', () => {
     </div>
             `
 
-    
-           const searchText = document.getElementById('search-text')
-            const api = `http://openlibrary.org/search.json?q=${searchText.value}`
-    
-            fetch(api)
+    // search value  
+    const searchText = document.getElementById('search-text')
+    if (searchText.value == '') {
+
+        bookContainer.innerHTML = ("Please search with a valid book name")
+    }
+    else {
+
+        const api = `https://openlibrary.org/search.json?q=${searchText.value}`
+        searchText.value = ''
+        fetch(api)
             .then(res => res.json())
             .then(data => showData(data.docs))
+    }
 
 })
+// error  handle  
+const showData = (books) => {
+    const bookArr = books.filter(book => book.cover_i !== undefined && book.author_name !== undefined && book.publisher !== undefined && book.title !== undefined && book.first_publish_year !== undefined)
 
-const showData =(books) => {
-    var bookArr = books.filter(book => book.cover_i !== undefined && book.author_name !== undefined && book.publisher !== undefined && book.title !== undefined && book.first_publish_year !== undefined)
-
-    if(bookArr.length === 0){
-        bookContainer.innerHTML = 'No Result Found'
-    }
-    else{
-        var bookDetails = document.getElementById('book-details')
-        var newP = document.createElement('p')
-        newP.innerHTML = `You got ${bookArr.length} books`
+    if (bookArr.length === 0) {
         bookDetails.innerHTML = ""
-        bookDetails.appendChild(newP)
-    }
+        bookContainer.innerHTML = ''
+        bookContainer.innerHTML = 'No Result Found'
+        return;
 
+
+    }
+    else {
+        const newParagraph = document.createElement('p')
+        newParagraph.innerHTML = ""
+        newParagraph.innerHTML = `You got ${bookArr.length} books`
+        bookDetails.innerHTML = ""
+        bookDetails.appendChild(newParagraph)
+
+
+    }
+    bookContainer.innerHTML = ''
     bookArr.forEach(book => {
         const newDiv = document.createElement('div')
         newDiv.innerHTML = `
@@ -41,19 +55,19 @@ const showData =(books) => {
                 <img class="card-img-top img-fluid"  style="width:300px; height:200px" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" >
             <div class="card-body overflow-hidden">
                 <h5 class="card-title text-primary">Book Title: ${book.title}</h5>
-                <p class="card-subtitle text-danger mb-2">Book Author Name: ${book.author_name[0]}</p>
-                <p class="card-text">Book Publisher: ${book.publisher[0]}</p>
-                <p class="card-text"> First Publish Year: ${book.first_publish_year} </p>
+                <p class="card-subtitle text-danger mb-2">Book Author Name : ${book.author_name[0]}</p>
+                <p class="card-text">Book Publisher : ${book.publisher[0]}</p>
+                <p class="card-text"> First Publish Year : ${book.first_publish_year} </p>
             </div>
             </div>
             `
 
-            bookContainer.appendChild(newDiv)
+        bookContainer.appendChild(newDiv)
     })
 
-    
 
 
-    
+
+
 }
 
